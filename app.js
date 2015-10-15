c46019ac8a913470176ca45bcd69df3a0cbbf1fa
ayyshi <ayyshi@gmail.com>
2015-10-15 17:12:52 -0400

    count stone amounts

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ var  renderBoard = function() {
     var button = document.createElement('button');
     button.setAttribute('id', 'pit'+i);
     //assign initial values to each button
-    button.setAttribute('value', gameBoard[i]);
+    // button.setAttribute('value', gameBoard[i]);
     button.innerHTML = gameBoard[i];
     row.appendChild(button);
   };
@@ -33,5 +33,62 @@ var  renderBoard = function() {
   playerClass[1].setAttribute('class','playerOne');
 };
 
+//get amount of stones in each pit from the text of the buttons
+var getStones = function(){
+  $('button').click(function() {
+  var amtStones;
+
+  switch ( $('button').index( this ) ) {
+    case 0 :
+      amtStones = Number($('#pit0').text());
+      break;
+    case 1 :
+      amtStones = Number($('#pit1').text());
+      break;
+    case 2 :
+      amtStones = Number($('#pit2').text());
+      break;
+    case 3 :
+      amtStones = Number($('#pit3').text());
+      break;
+    case 4 :
+      amtStones = Number($('#pit4').text());
+      break;
+    case 5 :
+      amtStones = Number($('#pit5').text());
+      break;
+    case 6 :
+      amtStones = Number($('#pit6').text());
+      break;
+    case 7 :
+      amtStones = Number($('#pit7').text());
+      break;
+    case 8 :
+      amtStones = Number($('#pit8').text());
+      break;
+    case 9 :
+      amtStones = Number($('#pit9').text());
+      break;
+    case 10 :
+      amtStones = Number($('#pit10').text());
+      break;
+    case 11 :
+      amtStones = Number($('#pit11').text());
+      break;
+  }
+});
+};
+
+
+var sowStones = function(amtStones){
+  if (amtStones === 0){
+    alert('invalid selection');
+  } else {
+    
+  }
+}
+
+//change to do not sow sow stone to banks
 
 renderBoard();
+getStones();
