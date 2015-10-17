710de9e8cef5026f0b5f13ff6eda1caf1f24b678
ayyshi <ayyshi@gmail.com>
2015-10-16 20:08:04 -0400

    changes to moveStone function

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 //global board with starting values in array
-var gameBoard = [
-  4,4,4,4,4,4,
-  4,4,4,4,4,4
-];
+var gameBoard = [0,1,2,3,4,5,6,7,8,9,10,11,12,13];
 
 //set turn as 0
 var turn = 0;
+
+//set stone count to 0
+var amtStones = 0;
 
 //render board
 var  renderBoard = function() {
@@ -13,7 +13,7 @@ var  renderBoard = function() {
 
   for(var i = 0; i < gameBoard.length; i++){
 
-    if(i % 6 === 0){
+    if(i % 7 === 0){
       var row = document.createElement('div');
       gameContainer.appendChild(row);
     };
@@ -21,6 +21,11 @@ var  renderBoard = function() {
     //add buttons to each row
     var button = document.createElement('button');
     button.setAttribute('id', 'pit'+i);
+    //add listener for all buttons and call getStones function
+    //assign to amtStones
+    button.addEventListener('click', function(){
+      amtStones = Number(this.textContent);
+    });
     //assign initial values to each button
     // button.setAttribute('value', gameBoard[i]);
     button.innerHTML = gameBoard[i];
@@ -33,62 +38,37 @@ var  renderBoard = function() {
   playerClass[1].setAttribute('class','playerOne');
 };
 
-//get amount of stones in each pit from the text of the buttons
-var getStones = function(){
-  $('button').click(function() {
-  var amtStones;
-
-  switch ( $('button').index( this ) ) {
-    case 0 :
-      amtStones = Number($('#pit0').text());
-      break;
-    case 1 :
-      amtStones = Number($('#pit1').text());
-      break;
-    case 2 :
-      amtStones = Number($('#pit2').text());
-      break;
-    case 3 :
-      amtStones = Number($('#pit3').text());
-      break;
-    case 4 :
-      amtStones = Number($('#pit4').text());
-      break;
-    case 5 :
-      amtStones = Number($('#pit5').text());
-      break;
-    case 6 :
-      amtStones = Number($('#pit6').text());
-      break;
-    case 7 :
-      amtStones = Number($('#pit7').text());
-      break;
-    case 8 :
-      amtStones = Number($('#pit8').text());
-      break;
-    case 9 :
-      amtStones = Number($('#pit9').text());
-      break;
-    case 10 :
-      amtStones = Number($('#pit10').text());
-      break;
-    case 11 :
-      amtStones = Number($('#pit11').text());
-      break;
-  }
-});
+var sowStones = function(amtStones,pitIndex){
+  if (amtStones === 0){
+    alert('invalid selection');
+  } else {
+    gameBoard.splice(pit, 1, 0);
+    for(i = 0; i < amtStones; i++){
+      gameBoard.splice(pit+1, 1, gameBoard[pit]+1);
+    }
+    return gameBoard;
+  };
 };
 
-
-// var sowStones = function(amtStones){
-//   if (amtStones === 0){
-//     alert('invalid selection');
-//   } else {
-//
-//   }
-// }
-
-//change to do not sow sow stone to banks
-
 renderBoard();
-getStones();
+sowStones();
+
+
+//using pitIndex to movestones ==== needs fixing
+var board = [0,1,2,3,4,5,6,7,8]
+function moveStones(pitIndex){
+  var stonesInHand = board[pitIndex];
+  board[pitIndex] = 0;
+  var i;
+  if(stonesInHand > 0){
+    for(i = pitIndex + 1; i < board.length; i++){
+      board[pitIndex] += 1;
+    }
+    return board;
+  } else {
+    alert('error');
+  }
+}
+
+
+///player must defined before game starts
