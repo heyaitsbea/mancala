e3b7060a68965741a0b556838457c67693226758
Bea Manaligod <bea.manaligod@gmail.com>
2020-04-08 02:02:27 -0400

    most recent build

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //global board with starting values in array
 var gameBoard = [
-  0,4,4,4,4,4,4,
-  0,4,4,4,4,4,4
+  0, 4, 4, 4, 4, 4, 4,
+  0, 4, 4, 4, 4, 4, 4
 ];
 
 var highlighted;
@@ -11,30 +11,31 @@ var highlighted;
 
 // use
 // make number/put ring around number = 
-  // when it starts off, put in the same starting spot
-    // if theres none in that, move over to the next spot
-  
+// when it starts off, put in the same starting spot
+// if theres none in that, move over to the next spot
+
 
 // computer AI
 
 //set currentPlayer as 1
-var currentPlayer = 'one'; 
+var currentPlayer = 'one';
 
 //game play readout
-var readOut = document.querySelector('div.info'); 
+var readOut = document.querySelector('div.info');
 
 function renderBoard() {
-  var gameContainer = document.querySelector('.container'); // render the container
-  for(var i = 0; i < gameBoard.length; i++){ // go through the length of the game board
 
-    if(i % 7 === 0){  // if you're in one of the two rows
+  var gameContainer = document.querySelector('.container'); // render the container
+  for (var i = 0; i < gameBoard.length; i++) { // go through the length of the game board
+
+    if (i % 7 === 0) {  // if you're in one of the two rows
       var row = document.createElement('div'); // create a div
       gameContainer.appendChild(row); // add the row to the board
     };
 
     //add buttons to each row
     var button = document.createElement('button'); // create a button representing each pit
-    button.setAttribute('class', 'pit'+i);  // each pit has a specific class depending on what it is
+    button.setAttribute('class', 'pit' + i);  // each pit has a specific class depending on what it is
     button.setAttribute('id', i); // each has an ID depending on what it is 
     //assign initial values to each button
     button.innerHTML = gameBoard[i]; // each pit = the value that is in that array
@@ -42,8 +43,8 @@ function renderBoard() {
   };
 
   var playerClass = document.querySelectorAll('.container div'); // select all
-  playerClass[0].setAttribute('class','playerTwo'); 
-  playerClass[1].setAttribute('class','playerOne');
+  playerClass[0].setAttribute('class', 'playerTwo');
+  playerClass[1].setAttribute('class', 'playerOne');
 
   setListeners();
 };
@@ -54,52 +55,168 @@ function renderBoard() {
 // if you move it to an empty space, you get what's in that space + the space across frmo you in your bank
 // it still goes into the bank of the opponent no matter what
 
-function moveStones(pitIndex){ // moving at a certain pit
-  console.log("pit index is " + pitIndex);
+function moveStones(pitIndex) { // moving at a certain pit
+
   var stonesInHand = gameBoard[pitIndex]; // the stones you have is the amount in the pit you pick up
   var pitNextIndex = pitIndex + 1; // finds next index 
   gameBoard[pitIndex] = 0; // setting the stones you have to 0, because you're picking all of them up
   var i;
-  if(stonesInHand > 0) { // if you click on a full pit
+  if (stonesInHand > 0) { // if you click on a full pit
 
-    for(i = pitNextIndex; i < stonesInHand + pitNextIndex; i++){ // for each 
+    for (i = pitNextIndex; i < stonesInHand + pitNextIndex; i++) { // for each 
       // starts at the next index, makes sure you use all stones, increments
       lastIndex = i % gameBoard.length; // last index  that something will be dropped (when everything is out!)
       // founds the index it would be dropped at
-      gameBoard[lastIndex] += 1; //
-      
-      
+
+      // supposed to skip the opponent's bank
+      if ((currentPlayer == "one" && lastIndex != 7) || (currentPlayer == "two" && lastIndex != 0)) {
+        gameBoard[lastIndex] += 1; //
+      }
+
     }
   };
 
-
   bankStones(lastIndex); //send the index
-
   if ((currentPlayer == 'one' && lastIndex != 0) || currentPlayer == 'two' && lastIndex != 7) {
-    setPlayer();
+
+    document.getElementById(highlighted).style.backgroundColor = "#DEB887"
+    document.getElementById(highlighted).style.border = "#8B7355"
+
+    setPlayer(); // next player's turn!
   } else { // don't change if they're in a bank
-    readOut.textContent = 'It is player '+currentPlayer+'\'s turn';
+    readOut.textContent = 'It is still player ' + currentPlayer + '\'s turn!';
+    if (currentPlayer == 'one') {
+
+
+      document.getElementById(highlighted).style.backgroundColor = "#DEB887"
+      document.getElementById(highlighted).style.border = "#8B7355"
+      highlighted = 8;
+
+      document.getElementById(highlighted).style.backgroundColor = "#abebb4";
+      document.getElementById(highlighted).style.border = "#aaa9ad";
+    } else if (currentPlayer == 'two') {
+
+      document.getElementById(highlighted).style.backgroundColor = "#DEB887"
+      document.getElementById(highlighted).style.border = "#8B7355"
+
+      highlighted = 1;
+
+      player2Turn(1);
+    }
   }
-  
+
 };
 
-// function player2Turn (pitIndex) { // where to get the pit
+// implemented an AI player to make gameplay more accessible
+function player2Turn(pitIndex) { // where to get the pit
 
-// }
+  // LOOP THROUGH ALL POSITIONS
+  // pitIndex = 7 - pitIndex; // get the one closest to the bank
+  var tempGameBoard = [];
+  var potentialTotal = []; // potential gain for each possible move
 
-function bankStones(lastIndex){   // takes in index  
+  for (let i = pitIndex; i < 7; i++) {
+    for (let j = 0; j < gameBoard.length; j++) { // reset the game board every time
+      tempGameBoard[j] = this.gameBoard[j];
+    }
+
+
+    var stonesInHand = tempGameBoard[i]; // check with these amounts
+
+    if (stonesInHand > 0) {
+
+      var pitNextIndex = i + 1; // finds next index 
+      tempGameBoard[i] = 0; // setting the stones you have to 0, because you're picking all of them up
+      var j;
+      for (j = i; j < stonesInHand + pitNextIndex; j++) { // for each 
+        // starts at the next index, makes sure you use all stones, increments
+        lastIndex = j % tempGameBoard.length; // last index  that something will be dropped (when everything is out!)
+        // founds the index it would be dropped at
+
+        // supposed to skip the opponent's bank
+        if ((currentPlayer == "one" && lastIndex != 7) || (currentPlayer == "two" && lastIndex != 0)) {
+          tempGameBoard[lastIndex] += 1; //
+        }
+
+      }
+
+      var inverse = tempGameBoard.length - lastIndex;  // inverse is the index ACROSS from it 
+      //if lastIndex is not a bank and has one stone
+      if ((lastIndex != 0) && (lastIndex != 7) && (tempGameBoard[lastIndex] === 1)) { // if there's only 1 in the last one AND is not a bank
+        if (currentPlayer === 'two') {   // if it's the opponent's turn
+          if (1 <= lastIndex <= 6) {
+            //move all stones in inverse pit to player 2 bank
+
+            tempGameBoard[7] = tempGameBoard[7] + tempGameBoard[inverse] + 1;
+            tempGameBoard[lastIndex] = 0; // clears out both
+            tempGameBoard[inverse] = 0;
+          };
+        }
+      };
+      potentialTotal[i] = tempGameBoard[7];
+    } else {
+      potentialTotal[i] = this.gameBoard[7]; // no gain.
+    }
+
+
+  }
+
+
+
+  var maxGain = 0;
+  var maxGainIndex = 1;
+  for (let i = 1; i < 7; i++) { // cycle through all the potential options
+
+
+    if (potentialTotal[i] > maxGain) { // this is potential gain
+      maxGain = potentialTotal[i];
+      maxGainIndex = i; // keep track of this 
+
+    }
+
+  }
+
+
+
+  var selectBtn = document.getElementById(maxGainIndex); // where are you gonna make the move?
+  highlighted = maxGainIndex;
+  var delayInMilliseconds = 3000; // 3 seconds
+
+  setTimeout(function () {
+    setTimeout(function () {
+      moveStones(maxGainIndex); // board
+
+    }, delayInMilliseconds)
+
+    selectBtn.style.backgroundColor = "#f56e5f"
+    selectBtn.style.border = "#aaa9ad";
+    document.getElementById("1").style.backgroundColor = "#DEB887";
+    document.getElementById("1").style.border = "#8B7355"
+
+
+  }, delayInMilliseconds);
+
+
+
+
+
+
+
+};
+
+function bankStones(lastIndex) {   // takes in index  
   var inverse = gameBoard.length - lastIndex;  // inverse is the index ACROSS from it 
   //if lastIndex is not a bank and has one stone
-  if((lastIndex != 0) && (lastIndex != 7) && (gameBoard[lastIndex] === 1)) { // if there's only 1 in the last one AND is not a bank
-    if(currentPlayer === 'two'){   // if it's the opponent's turn
-      if(1 <= lastIndex <= 6){  
+  if ((lastIndex != 0) && (lastIndex != 7) && (gameBoard[lastIndex] === 1)) { // if there's only 1 in the last one AND is not a bank
+    if (currentPlayer === 'two') {   // if it's the opponent's turn
+      if (1 <= lastIndex <= 6) {
         //move all stones in inverse pit to player 2 bank
-        gameBoard[7] = gameBoard[7] + gameBoard[inverse] + 1;  
+        gameBoard[7] = gameBoard[7] + gameBoard[inverse] + 1;
         gameBoard[lastIndex] = 0; // clears out both
         gameBoard[inverse] = 0;
       };
     } else { // if it is Your turn
-      if(8 <= lastIndex <= 13){
+      if (8 <= lastIndex <= 13) {
         gameBoard[0] = gameBoard[0] + gameBoard[inverse] + 1;
         gameBoard[lastIndex] = 0;
         gameBoard[inverse] = 0;
@@ -110,8 +227,8 @@ function bankStones(lastIndex){   // takes in index
   updateBoard();
 };
 
-var updateBoard = function (){ // update the board
-  for(var i = 0; i < gameBoard.length; i++){
+var updateBoard = function () { // update the board
+  for (var i = 0; i < gameBoard.length; i++) {
     var pit = document.querySelectorAll('button');
     pit[i].textContent = gameBoard[i];
   };
@@ -120,41 +237,63 @@ var updateBoard = function (){ // update the board
 };
 
 // winning = using up all the stones on your side
-function checkWin(){  // see if someone won
-  var playerOneStones = gameBoard[1]+gameBoard[2]+gameBoard[3]+gameBoard[4]+gameBoard[5]+gameBoard[6];
-  var playerTwoStones = gameBoard[8]+gameBoard[9]+gameBoard[10]+gameBoard[11]+gameBoard[12]+gameBoard[13];
-  if(playerOneStones === 0 || playerTwoStones === 0){
+function checkWin() {  // see if someone won
+   console.log("Check for winner");
+
+  var playerOneStones = gameBoard[1] + gameBoard[2] + gameBoard[3] + gameBoard[4] + gameBoard[5] + gameBoard[6];
+  var playerTwoStones = gameBoard[8] + gameBoard[9] + gameBoard[10] + gameBoard[11] + gameBoard[12] + gameBoard[13];
+  console.log(playerOneStones);
+  console.log(playerTwoStones);
+
+  if (playerOneStones === 0 || playerTwoStones === 0) {
     var playerOneTotal = playerOneStones + gameBoard[0];
     var playerTwoTotal = playerTwoStones + gameBoard[7];
 
-    if(playerOneTotal > playerTwoTotal){
+    if (playerOneTotal > playerTwoTotal) {
+      alert('Player One Wins!');
       readOut.textContent = 'Player One Wins!';
+      window.location.href = window.location.href;
     } else {
+      alert(readOut.textContent = 'Player Two Wins!');
       readOut.textContent = 'Player Two Wins!';
+      window.location.href = window.location.href;
+  ;
+      
     };
   };
 };
 
 //set current player
-function setPlayer(){ // set the player
-  if(currentPlayer === 'one'){ // switch the string holding the current player
+function setPlayer() { // set the player
+  if (currentPlayer === 'one') { // switch the string holding the current player
+    highlighted = 1;
     currentPlayer = 'two';
-    readOut.textContent = 'It is player '+currentPlayer+'\'s turn'; // text to print
+    readOut.textContent = 'It is player ' + currentPlayer + '\'s turn'; // text to print
     var deselectBtn = document.getElementById(highlighted);
-    
-    deselectBtn.style.backgroundColor = "#DEB887"; // oG color
+
+    deselectBtn.style.backgroundColor = "#DEB887"; // oG color and border
     deselectBtn.style.border = "#8B7355";
 
     var selectBtn = document.getElementById("1");
-    
-    // go to AI function
 
-
-    return currentPlayer;  
+    // selectBtn.style.backgroundColor = "#f56e5f"
+    // selectBtn.style.border = "#aaa9ad";
+    player2Turn(1);
+    return currentPlayer;
   } else {
-    currentPlayer = 'one'; 
-    readOut.textContent = 'It is player '+currentPlayer+'\'s turn';
-    
+    currentPlayer = 'one';
+    highlighted = 8;
+    readOut.textContent = 'It is player ' + currentPlayer + '\'s turn';
+    var deselectBtn = document.getElementById(highlighted);
+
+    deselectBtn.style.backgroundColor = "#DEB887"; // oG color and border
+    deselectBtn.style.border = "#8B7355";
+
+
+
+    var selectBtn = document.getElementById("8");
+    selectBtn.style.backgroundColor = "#abebb4";
+    selectBtn.style.border = "#aaa9ad";
     return currentPlayer;
   };
 
@@ -162,63 +301,55 @@ function setPlayer(){ // set the player
 
 
 function moveStones2(e) { // when key is pressed,, call this
-
-  console.log("hi");
   // check to see which keys
- var keyCode = e.keyCode;
-  if (keyCode == 13) { // CONFIRM SELECTION
+  var keyCode = e.keyCode;
+  if (keyCode == 13 && currentPlayer == 'one') { // CONFIRM SELECTION
     moveStones(highlighted);
     // call moveStones
     // get current value of the index
     // how to get the current number we're in?
     // send current value of the pitIndex somehow
-  } else if (keyCode == 32) { // go through options
-    
+  } else if (keyCode == 32 && currentPlayer == 'one') { // go through options
+
     var prevHighlighted = highlighted; // keep in here
 
-    console.log("at the start, highlighted is " + highlighted);
 
-    if (highlighted + 1 != 7 && highlighted + 1 <= 13) { // if its within the bounds to be selected
-      highlighted ++; // if its in the bounds go ahead and increase it
+    if (highlighted + 1 <= 13) { // if its within the bounds to be selected
+      highlighted++; // if its in the bounds go ahead and increase it
     } else {
-      if (highlighted + 1 == 7) {
+      if (highlighted + 1 == 14) {
+        //  console.log("next step is " + highlighted + 1);
         highlighted = 8
-      } else if (highlighted + 1 >= 13) {
-        highlighted = 1;
       }
     }
 
     // need to increment the variable that you currently are on
     var prevSpot = document.getElementById(prevHighlighted);
-    var selectedSpot  = document.getElementById(highlighted);
-
-
+    var selectedSpot = document.getElementById(highlighted);
     prevSpot.style.backgroundColor = "#DEB887"; // oG color
     prevSpot.style.border = "#8B7355";
 
-    selectedSpot.style.backgroundColor = "#abebb4"; // oG color
+    selectedSpot.style.backgroundColor = "#abebb4";
     selectedSpot.style.border = "#aaa9ad";
-    
-
-   
   }
 }
 
-var setListeners = function(){   // only sets at the beginning
+var setListeners = function () {   // only sets at the beginning
   // 
   document.addEventListener("keydown", moveStones2, false);
 
-  for(var i = 0; i < gameBoard.length; i++){  //setting a listener for each pit
+  for (var i = 0; i < gameBoard.length; i++) {  //setting a listener for each pit
     var pit = document.querySelectorAll('button'); // select all the stones to be listening for a click
 
-    pit[1].style.backgroundColor =  "#abebb4";
-    pit[1].style.border =  "#aaa9ad";
-    highlighted = 1;
+    pit[8].style.backgroundColor = "#abebb4";
+    pit[8].style.border = "#aaa9ad";
+    highlighted = 8;
 
-    pit[i].addEventListener('click', function(eventObject){moveStones(Number(eventObject.target.id))} );
+    pit[i].addEventListener('click', function (eventObject) { moveStones(Number(eventObject.target.id)) });
     // moves stones when te button 
     // moveStones
-  }; 
+  };
 };
 
 renderBoard();
+
